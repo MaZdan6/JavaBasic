a60192cb65e55e64628ca4efbdd111cae6fbe886
Mateusz Zdanowicz <fazol66@gmail.com>
2019-03-18 23:29:55 +0100

    refactor

--- src/main/java/org/zdanek/coursera/CeasarCipher.java
+++ src/main/java/org/zdanek/coursera/CeasarCipher.java
@@ -2,33 +2,45 @@ package org.zdanek.coursera;
 
 public class CeasarCipher {
 	
-		static String alphabet="abcdefghijklmnopqrstuvwxyz";
+	String alphabet="abcdefghijklmnopqrstuvwxyz";
 	
 	public static void main(String[] args) {
-		
-		String text="a bat";
-		int p=19;
+		CeasarCipher cipher=new CeasarCipher();
+		String text="At noon be in the conference room with your hat on for a surprise party. YELL LOUD!";
+		int key=15;
 		System.out.println(text);
 		
-		String encryptedText=encryptText(text,p);
+		String encryptedText=cipher.encryptText(text,key);
 		System.out.println(encryptedText);
 	}
 	
-	private static String encryptText(String text, int p) {
+	private String encryptText(String text, int key) {
 		
+		String encryptedText = encryptAlghoritm(text, key,false);
+		encryptedText = encryptAlghoritm(encryptedText, key,true);
+		return encryptedText;
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private String encryptAlghoritm(String text, int key, boolean uppercase) {
+		if(uppercase) {
+			alphabet=alphabet.toUpperCase();
+		}
 		StringBuilder sb= new StringBuilder("");
-		String shiftedAlphabet= shiftAlphabet(p);
+		String shiftedAlphabet= shiftAlphabet(key);
 		System.out.println(alphabet);
 		System.out.println(shiftedAlphabet);
 		
 		char[] chars=text.toCharArray();
 		char encryptedChar=' ';
 		for(char i:chars) {
-			int alphabetIndex= alphabet.indexOf(i);
-			if(alphabetIndex==-1) {
-				encryptedChar=' ';
+			int index= alphabet.indexOf(i);
+			if(index==-1) {
+				encryptedChar=i;
 			}else {
-				encryptedChar= shiftedAlphabet.charAt(alphabetIndex);
+				encryptedChar= shiftedAlphabet.charAt(index);
 			}
 			
 			sb.append(encryptedChar);
@@ -36,11 +48,9 @@ public class CeasarCipher {
 		
 		String encryptedText=sb.toString();
 		return encryptedText;
-		// TODO Auto-generated method stub
-		
 	}
 
-	private static String shiftAlphabet(int p) {
+	private String shiftAlphabet(int p) {
 		
 		String shiftedAphabet= alphabet.substring(p);
 		shiftedAphabet=shiftedAphabet+alphabet.substring(0, p);
